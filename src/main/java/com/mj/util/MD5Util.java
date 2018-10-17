0f728fb102ea9f30f71d108bba6b9add4f104bc0
mj <mj@mj>
2018-10-17 10:21:10 +0800

    xiugai

--- src/main/java/com/mj/util/MD5Util.java
+++ src/main/java/com/mj/util/MD5Util.java
@@ -1,11 +1,8 @@
 package com.mj.util;
 import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import org.apache.shiro.crypto.hash.SimpleHash;
-
-import sun.misc.BASE64Encoder;
 
 public class MD5Util {
     /*MD5加密方法*/
@@ -16,10 +13,5 @@ public class MD5Util {
     	String encodedPassword = hash.toHex();
     	 System.out.println("result:"+hash.getSalt());
         return hash;
-    }
-
-    public static void main(String[] args) throws Exception{
-//        String result = MD5Util.encryptPassword("123","123",3);
-//        System.out.println("result:"+result);
     }
 }
